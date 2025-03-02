24fea2997e402e13227230be3885cde18aa84a61
Rick Flagg <richard.lee.flagg@outlook.com>
2025-03-02 09:41:08 -0500

    Fixing unit-tests

--- backend/tests/test_database.py
+++ backend/tests/test_database.py
@@ -1,8 +1,21 @@
+from config import AppSettings
 from database import VirginiaLandmarkRepository, get_context_session
+from database import ensure_database_created
+from pipelines import VirginiaLandmarkPipeline
 import unittest
 
 class VirginiaLandmarkRepositoryTest(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        #ensure we are setup and have data
+        ensure_database_created()
+
+        settings = AppSettings()
+        
+        with get_context_session() as db:
+            VirginiaLandmarkPipeline().injest(settings=settings, db=db)
+        
 
     def test_get_location_counties(self):
         with get_context_session() as db:
